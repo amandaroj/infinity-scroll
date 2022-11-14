e0cfe13004b30c69236445f3b5b278b97d0b5b2c
amanda rojter <rojteramanda@gmail.com>
2022-11-14 10:54:36 -0500

    scroll function and loader

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
+let initialLoad = true;
+
 //Unsplash API
-const count = 10;
+const count = 5;
 const apiKey = 'DlPj0HjV8AiZ2cELg4GY3osVZj8uAE3jSNLsv2Q34Ys';
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&topics=animal,cats&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&collections=animal,cats&count=${count}`;
+
+//check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+    count = 30;
+  }
+}
 
 //create elements for links and photos, add to DOM
 function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  console.log('total images', totalImages);
   photosArray.forEach(photo => {
     //create <a> element to link to unspash
     const item = document.createElement('a');
@@ -17,9 +35,12 @@ function displayPhotos() {
     //create image for photo
     const img = document.createElement('img');
     img.setAttribute('src', photo.urls.regular);
-    img.setAttribute('alt', photo.alt_description);
-    img.setAttribute('title', photo.alt_description);
-    //put img inside <a> element, then put both inside imageContainer element
+    img.setAttribute('alt', photo.description);
+    // added if statement to prevent null from appearing as title
+    if (photo.alt_description) img.setAttribute('title', photo.description);
+    //put img inside <a> element, then put both inside imageContainer
+    // event listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
@@ -29,11 +50,22 @@ async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = await response.json();
+    console.log(photosArray);
     displayPhotos();
   } catch (error) {
     //catch error here
   }
 }
 
+// check to see if scroll is at bottom of the page
+window.addEventListener('scroll', () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    getPhotos();
+    ready = false;
+  }
+});
 //On Load
 getPhotos();
